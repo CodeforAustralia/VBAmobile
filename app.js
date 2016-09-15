7d382f5105fb73eb9ca4ecadcc7d4df1a7ba8320
Johan Codinha <johan@codinha.net>
2016-09-16 00:06:00 +1000

    prototype ready to deploy

--- app.js
+++ app.js
@@ -1,99 +1,53 @@
-//Load the request module
-var request = require('request');
-var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+var exphbs  = require('express-handlebars');
+var path = require('path');
+var Session = require('./session');
 
-function Session() {
-	this.urls = {
-		login: 'https://vba.dse.vic.gov.au/vba/login',
-		myProjects: 'https://vba.dse.vic.gov.au/vba/vba/sc/IDACall?isc_rpc=1&isc_v=SC_SNAPSHOT-2010-08-03&isc_xhr=1&isc_tnum='+ 0,
-		allProjects: 'https://vba.dse.vic.gov.au/vba/vba/sc/IDACall?isc_rpc=1&isc_v=SC_SNAPSHOT-2010-08-03&isc_xhr=1&isc_tnum=7'
-	};
-	this.headers =  {
-		'Host': 'vba.dse.vic.gov.au',
-		'Connection': 'keep-alive',
-		// 'Content-Length': '37',
-		'Cache-Control': 'max-age=0',
-		'Origin': 'https://vba.dse.vic.gov.au',
-		'Upgrade-Insecure-Requests': '1',
-		// 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
-		// 'Content-Type': 'application/x-www-form-urlencoded',
-		// 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-		// 'Referer': 'https://vba.dse.vic.gov.au/vba/login.jsp',
-		// 'Accept-Encoding': 'gzip, deflate, br',
-		// 'Accept-Language': 'en-US,en;q=0.8,fr;q=0.6',
-	}
+var app = express();
 
-	this.jar = request.jar();
-	this.login = function() {
-		var me = this;
-		//Lets configure and request
-		request({
-				jar: this.jar,
-				url: this.urls.login, //URLs to hit
-				method: 'POST',
-				headers: this.headers,
-				form: {
-					username: 'codeforvic',
-					password: '19910908'
-				}
-			},
-			function(error, response, body) {
-				if (error) {
-					console.log(error);
-				} else {
-					console.log("login response code : " + response.statusCode);
-					// var cookie_string = me.jar.getCookieString(me.urls.login); // "key1=value1; key2=value2; ..."
-					var cookies = me.jar.getCookies(me.urls.login);
-					console.log(cookies);
-					debugger;
-					me.getProject();
-				}
-			});
-	}
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-	this.getProject = function() {
-		// console.log(this.jar.getCookies(this.urls.login).length)
-		var me = this;
-		var cookies = me.jar.getCookies(me.urls.login)
-		var headers = this.headers;
-		headers.Cookie = cookies[0];
+app.use(bodyParser.json());         
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('views'));
 
-		var myProject = '<isMyProjectSearch xsi:type="xsd:boolean">true</isMyProjectSearch>'
-		
-		// console.log(headers);
-		request({
-				// jar: this.jar,
-				url: this.urls.myProjects, //URL to hit
-				method: 'POST',
-				headers: headers,
-				form: {
-					_transaction: '<transaction xmlns:xsi="http://www.w3.org/2000/10/XMLSchema-instance" xsi:type="xsd:Object"><operations xsi:type="xsd:List"><elem xsi:type="xsd:Object"><criteria xsi:type="xsd:Object">'
-					+ myProject +
-					'<projectStatusCde>pub</projectStatusCde></criteria><operationConfig xsi:type="xsd:Object"><dataSource>Project_DS</dataSource><operationType>fetch</operationType><textMatchStyle>exact</textMatchStyle></operationConfig><startRow xsi:type="xsd:long">0</startRow><endRow xsi:type="xsd:long">75</endRow><componentId>isc_ManageProjectModule$2_0</componentId><appID>builtinApplication</appID><operation>mainProjectSearch</operation><oldValues xsi:type="xsd:Object"><projectStatusCde>pub</projectStatusCde><isMyProjectSearch xsi:type="xsd:boolean">true</isMyProjectSearch></oldValues></elem></operations></transaction>',
-					// This one for all Projects accesible to read by this account.
-					// _transaction: '<transaction xmlns:xsi="http://www.w3.org/2000/10/XMLSchema-instance" xsi:type="xsd:Object"><operations xsi:type="xsd:List"><elem xsi:type="xsd:Object"><criteria xsi:type="xsd:Object"><projectStatusCde>pub</projectStatusCde></criteria><operationConfig xsi:type="xsd:Object"><dataSource>Project_DS</dataSource><operationType>fetch</operationType><textMatchStyle>exact</textMatchStyle></operationConfig><startRow xsi:type="xsd:long">0</startRow><endRow xsi:type="xsd:long">75</endRow><componentId>isc_ManageProjectModule$2_0</componentId><appID>builtinApplication</appID><operation>mainProjectSearch</operation><oldValues xsi:type="xsd:Object"><projectStatusCde>pub</projectStatusCde></oldValues></elem></operations></transaction>' ,
-					protocolVersion: '1.0'
-				}
-			},
-			function(error, response, body) {
-				if (error) {
-					console.log(error);
-				} else {
-					// console.log(body);
-					console.log("get project response code : " + response.statusCode);
-					var trimBody = body.slice(25, -20);
-					trimBody = trimBody.replace(/Date\.parseServerDate/g, 'Date');
-					var data = eval(trimBody)[0];
-					fs.writeFile("trimbody.json", JSON.stringify(data), function(err) {
-						if (err) {
-							return console.log(err);
-						}
-						console.log("The file was saved!");
-					});
-				}
-			});
-	}
-
-};
 var session = new Session();
-session.login();
+
+app.get('/', function(req, res) {
+	console.log('logged in as ' + session.username);
+	res.render('index',{
+  		loggedIn : session.loggedIn,
+  		helpers : {
+  			username : session.username
+  		}
+  	});
+  // res.sendFile(path.join(__dirname + '/vbamobile.webflow/index.html'));
+});
+
+app.get('/login', function(req, res) {
+	res.render('login',{
+  		loggedIn : session.loggedIn,
+  		helpers : {
+  			username : session.username
+  		}
+  	});
+});
+
+app.post('/login', function (req, res) {
+  // var session = new Session();
+  console.log('Login request for' + req.body.username);
+  var credential = {
+  	username : req.body.username,
+  	password : req.body.password
+  }
+  var expressRes = function (){
+  	res.redirect('/');
+  }
+  session.login(credential, expressRes);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
---
+++ session.js
@@ -0,0 +1,115 @@
+//Load the request module
+var request = require('request');
+var fs = require('fs');
+
+function Session() {
+	this.loggedIn = false;
+	this.username = '';
+
+	this.urls = {
+		login: 		'https://vba.dse.vic.gov.au/vba/login',
+		projects: 'https://vba.dse.vic.gov.au/vba/vba/sc/IDACall?isc_rpc=1&isc_v=SC_SNAPSHOT-2010-08-03&isc_xhr=1',
+	};
+	
+	this.headers =  {
+		'Host': 'vba.dse.vic.gov.au',
+		'Connection': 'keep-alive',
+		'Cache-Control': 'max-age=0',
+		'Origin': 'https://vba.dse.vic.gov.au',
+		'Upgrade-Insecure-Requests': '1',
+		// 'Content-Length': '37',
+		// 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36',
+		// 'Content-Type': 'application/x-www-form-urlencoded',
+		// 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+		// 'Referer': 'https://vba.dse.vic.gov.au/vba/login.jsp',
+		// 'Accept-Encoding': 'gzip, deflate, br',
+		// 'Accept-Language': 'en-US,en;q=0.8,fr;q=0.6',
+	}
+
+	this.jar = request.jar();
+
+	this.login = function(credential, expressRes) {
+		var me = this;
+		var username = credential.username;
+		console.log(credential);
+		//Lets configure and request
+		request({
+				jar: this.jar,
+				url: this.urls.login, //URLs to hit
+				method: 'POST',
+				headers: this.headers,
+				form: {
+					username: credential.username,
+					password: credential.password
+				}
+			},
+			function(error, response, body) {
+				if (error) {
+					console.log(error);
+					expressRes();
+					return false;
+
+				} else {
+					console.log("login response code : " + response.statusCode);
+					// var cookie_string = me.jar.getCookieString(me.urls.login); // "key1=value1; key2=value2; ..."
+					var cookies = me.jar.getCookies(me.urls.login);
+					console.log(cookies);
+					me.loggedIn = true;
+					console.log(username);
+					me.username = username;
+					expressRes();
+					return true;
+					// debugger;
+					// me.getProject();
+				}
+			});
+
+	}
+
+	this.getProject = function() {
+		// console.log(this.jar.getCookies(this.urls.login).length)
+		var me = this;
+		var cookies = me.jar.getCookies(me.urls.login)
+		var headers = this.headers;
+		headers.Cookie = cookies[0];
+
+		var myProject = '<isMyProjectSearch xsi:type="xsd:boolean">true</isMyProjectSearch>';
+		// <isMyProjectSearch xsi:type="xsd:boolean">true</isMyProjectSearch>
+		
+		// console.log(headers);
+		request({
+				jar: this.jar,
+				url: this.urls.projects, //URL to hit
+				method: 'POST',
+				headers: headers,
+				form: {
+					_transaction: '<transaction xmlns:xsi="http://www.w3.org/2000/10/XMLSchema-instance" xsi:type="xsd:Object"><operations xsi:type="xsd:List"><elem xsi:type="xsd:Object"><criteria xsi:type="xsd:Object">'
+					+ myProject +
+					'<projectStatusCde>pub</projectStatusCde></criteria><operationConfig xsi:type="xsd:Object"><dataSource>Project_DS</dataSource><operationType>fetch</operationType><textMatchStyle>exact</textMatchStyle></operationConfig><startRow xsi:type="xsd:long">0</startRow><endRow xsi:type="xsd:long">75</endRow><componentId>isc_ManageProjectModule$2_0</componentId><appID>builtinApplication</appID><operation>mainProjectSearch</operation><oldValues xsi:type="xsd:Object"><projectStatusCde>pub</projectStatusCde><isMyProjectSearch xsi:type="xsd:boolean">true</isMyProjectSearch></oldValues></elem></operations></transaction>',
+					protocolVersion: '1.0'
+				}
+			},
+			function(error, response, body) {
+				if (error) {
+					console.log(error);
+				} else {
+					// console.log(body);
+					console.log("get project response code : " + response.statusCode);
+					var trimBody = body.slice(25, -20);
+					trimBody = trimBody.replace(/Date\.parseServerDate/g, 'Date');
+					var data = eval(trimBody)[0];
+
+					// fs.writeFile("trimbody.json", JSON.stringify(data), function(err) {
+					// 	if (err) {
+					// 		return console.log(err);
+					// 	}
+					// 	console.log("The file was saved!");
+					// });
+				}
+			});
+	}
+};
+
+// var session = new Session();
+// session.login();
+module.exports = Session;
