702c0bc84acf47421d85414beae8e27cae2ee9e5
Johan Codinha <johan@codinha.net>
2017-02-01 17:57:59 +1100

    Species name is now displayed when filling up autocomplete form

--- views/js/awesomplete-taxon.js
+++ views/js/awesomplete-taxon.js
@@ -1,22 +1,32 @@
-var speciesListApi = "https://vbaspecies.herokuapp.com/species/search?q=";
-var ajax = new XMLHttpRequest();
-var input = document.getElementById("taxonId");
-var awesomplete = new Awesomplete(input, {
-		  minChars: 1,
-		});
+(function(){
+	let speciesListApi = "https://vbaspecies.herokuapp.com/species/search?q=";
+	let ajax = new XMLHttpRequest();
+	let taxonId = document.getElementById("taxonId");
+	let taxonName = document.getElementById("taxonName");
+	let awesomplete = new Awesomplete(taxonName, {
+			  minChars: 1,
+			});
 
-input.addEventListener('keyup', function(event) {
-	let e = event || window.event;
-	console.log(e) 
-		if ((e.keyCode >= 46) && (e.keyCode <= 90)) {
-			console.log(e.keyCode)
-			ajax.open("GET", speciesListApi + input.value, true);
-			ajax.onload = function() {
-				var list = JSON.parse(ajax.responseText).map(function(i) { 
-					return [i.COMMON_NAME + " - " + i.SCIENTIFIC_NAME , i.TAXON_ID]; });
-				console.log(list)
-				awesomplete.list = list;
-		};
-		ajax.send();
-	}
-});
+	taxonName.addEventListener('keyup', function(event) {
+		let e = event || window.event;
+		console.log(e) 
+			if ((e.keyCode >= 46) && (e.keyCode <= 90)) {
+				// console.log(e.keyCode)
+				ajax.open("GET", speciesListApi + taxonName.value, true);
+				ajax.onload = function() {
+					let list = JSON.parse(ajax.responseText).map(function(i) { 
+						return (i.COMMON_NAME ? i.COMMON_NAME + " - " : '') + i.SCIENTIFIC_NAME + ' #'+ i.TAXON_ID; 
+					});
+					console.log(list)
+					awesomplete.list = list;
+			};
+			ajax.send();
+		}
+	});
+
+	document.addEventListener('awesomplete-selectcomplete', function(event){
+		let extractedTaxonId = /#(\d*)/.exec(event.text.value)[1];
+		taxonId.value = extractedTaxonId;
+		console.log( extractedTaxonId, ' selected' );
+	});
+}());
